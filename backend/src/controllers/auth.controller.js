7a460e0bacf44091dcf7a168e6b611406b0756df
aditya-1408 <1408asingh@gmail.com>
2026-04-14 04:01:46 +0530

    add login and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -46,10 +46,37 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login route");
+export const login = async (req, res) => {
+  const {email,password}=req.body;
+  try{
+    const user = await User.findOne({email})
+
+    if(!user){
+        return res.status(400).json({message:"Invalid email or password"})
+    }
+    const isPasswordMatch = await bcrypt.compare(password,user.password);
+    if(!isPasswordMatch){
+        return res.status(400).json({message:"Invalid email or password"})
+    }
+    generateToken(user._id,res);
+    res.status(200).json({
+        _id:user._id,
+        fullName:user.fullName,
+        email:user.email,
+        profilePic:user.profilePic
+    })
+  } catch(error){
+    console.error("Error in login:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("Logout route");
+ try{
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({message:"Logged out successfully"})
+ } catch(error){
+    console.error("Error in logout:", error);
+    res.status(500).json({ message: "Server error" });
+ }
 };
