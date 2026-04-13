c2c2075a5fa7ab66697b8c025c092f56aa27d2ac
aditya-1408 <1408asingh@gmail.com>
2026-04-14 03:33:39 +0530

    generate token

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,14 +1,14 @@
-import jwt from "jsonwebtoken";  // Import the jsonwebtoken library for handling JWTs
-export const generateToken = (userId,res)=>{
-  const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "7d"});
+import jwt from "jsonwebtoken"; // Import the jsonwebtoken library for handling JWTs
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-
-  res.cookie("jwt",token,{
-    httpOnly: true,// cookie is only accessible by the server
+  res.cookie("jwt", token, {
+    httpOnly: true, // cookie is only accessible by the server
     secure: process.env.NODE_ENV !== "development", // cookie is only sent over HTTPS in production
     sameSite: "strict", // cookie is only sent for same-site requests
     maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expires in 7 days
-  })
+  });
   return token;
-}
-
+};
