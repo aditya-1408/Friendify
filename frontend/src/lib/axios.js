1704da5f928983ac2f7ff0159d6bf11564ab6995
aditya-1408 <1408asingh@gmail.com>
2026-04-14 20:48:53 +0530

    done finally

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from 'axios';  // Import the axios library for making HTTP requests,axios is a promise-based HTTP client for the browser and Node.js.
+import axios from "axios";
 
-export const axiosInstance = axios.create({ // 
-    baseURL: 'http://localhost:5001/api',
-    withCredentials: true,
-}) // Create an instance of axios with a base URL and withCredentials set to true. This instance can be used throughout the application to make HTTP requests to the specified base URL, and it will include credentials (like cookies) in the requests.
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
+  withCredentials: true,
+});
